f7bf302c91d8cf087b859a9eeaa47973f4486d7f
Matthew Zenn <spyguy57@gmail.com>
2021-06-03 13:11:23 -0400

    Added files

---
+++ index.js
@@ -0,0 +1,21 @@
+var title = document.getElementById('title').innerHTML;
+var maximize = document.getElementById("maximize")
+
+document.getElementById('titleShown').innerHTML = title;
+maximize.addEventListener("click", Maximize)
+
+if (window.innerWidth == screen.width) {
+    document.getElementById("circle").innerHTML = "&triangledown;"
+}
+
+function Maximize() {
+    if (window.innerWidth == screen.width) {
+        window.resizeTo(1280, 720);
+        document.getElementById("circle").innerHTML = "&triangle;"
+    }
+    else {
+        window.moveTo(0, 0);
+        window.resizeTo(screen.width, screen.height);
+        document.getElementById("circle").innerHTML = "&triangledown;"
+    }
+}
---
+++ main.js
@@ -0,0 +1,18 @@
+const electron = require('electron');
+const remote = electron.remote
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+Menu.setApplicationMenu(null);
+
+let mainWindow;
+
+app.on('ready', function(){
+    mainWindow = new BrowserWindow({ width: 1280, height: 720, resizable: true, frame: false, icon: "icon.ico"});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'window.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+});
